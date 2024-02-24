ce6ef287f7ed714e1df6187c5ebf7810c532321b
Sergey Makletsov <smaklets@kpfu.ru>
2024-02-24 21:23:25 +0300

    реализация добавления/удаления элементов

--- Queue2024/queue.cpp
+++ Queue2024/queue.cpp
@@ -1,1 +1,28 @@
 #include "queue.h"
+#include <string.h>
+
+void enqueue(queue& q, const char* name, int age)
+{
+	auto* new_el = new queue_elem;
+	new_el->age = age;
+	strcpy_s(new_el->name, name);
+	new_el->prev = q.last;
+	if (q.last) q.last->next = new_el;
+	q.last = new_el;
+	if (!q.first) q.first = new_el;
+}
+
+bool dequeue(queue& q, char*& name, int& age)
+{
+	if (!q.first) return false;
+	delete[] name;
+	name = new char[strlen(q.first->name) + 1];
+	strcpy_s(name, strlen(q.first->name) + 1, q.first->name);
+	age = q.first->age;
+	queue_elem* del = q.first;
+	q.first = q.first->next;
+	if (q.first) q.first->prev = nullptr;
+	else q.last = nullptr;
+	delete del;
+	return true;
+}
