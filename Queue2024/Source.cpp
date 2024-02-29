0a413cd470fcc5dffe1dbb0e26a7f152a7556f0e
Sergey Makletsov <smaklets@kpfu.ru>
2024-02-29 10:40:38 +0300

    Демо-пример

--- Queue2024/Source.cpp
+++ Queue2024/Source.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <fstream>
 #include "queue.h"
 using namespace std;
 
+void show_queue_data(queue q)
+{
+	auto* curr = q.first;
+	while (curr)
+	{
+		cout << curr->name << " " << curr->age << "\n";
+		curr = curr->next;
+	}
+	cout << endl;
+}
+
 void main()
 {
-	
+	queue trud;
+	queue ne_trud;
+
+	ifstream file("persons.txt");
+	if (file)
+	{
+		char* name = new char[256];
+		int age;
+		while (!file.eof())
+		{
+			file >> name;
+			file >> age;
+			enqueue(age >= 14 && age < 65 ? trud : ne_trud, name, age);
+		}
+		file.close();
+	} else
+	{
+		cout << "Файл с данными не открылся :(";
+	}
+	cout << "Трудоспособные: \n";
+	show_queue_data(trud);
+	cout << "Нетрудоспособные: \n";
+	show_queue_data(ne_trud);
+
+	clear(trud);
+	clear(ne_trud);
 }
